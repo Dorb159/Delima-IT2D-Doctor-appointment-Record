bf45bdeccb02dd190ccf0f1b156e958c74f24b7f
Delima <rhexzel1@gmail.com>
2024-11-07 14:53:19 +0800

    Add files via upload

---
+++ DAR-IT2D/src/dar/it2d/Patient.java
@@ -0,0 +1,36 @@
+package dar.it2d;
+
+import java.util.Scanner;
+
+
+public class Patient {
+    
+    public void cTransaction(){
+        
+        Scanner sc = new Scanner(System.in);
+        
+    System.out.println("Customer Panel");
+    System.out.println("ADD PATIENT");
+    System.out.println("VIEW PATIENT");
+    System.out.println("UPDATE PATIENT");    
+    System.out.println("DELETE PATIENT");
+    System.out.println("EXIT");
+    
+    System.out.println("Enter Selection: ");
+    int act = sc.hashCode();
+    
+    switch(act){
+    
+    case 1:
+                  
+      break;
+    case 2:
+        
+      break;
+         
+            
+            
+    }
+    
+}
+}
